79ab2ae5b095e2c2465e282d6b2688b94d343b26
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-15 14:00:12 +0100

    add get requests

--- mongoose/server.js
+++ mongoose/server.js
@@ -13,21 +13,48 @@ async function insertUser(userData = {}) {
   const user = new User(userData);
 
   try {
-      const result = await user.save();
+    const result = await user.save();
     console.log("User created: ", result);
   } catch (error) {
     console.log("ERROR: user not created", error);
   }
 }
 
-//TODO: get all users
-//TODO: get all users with name [NAME]
+
+async function getUsers(name = "") {
+    try {
+        let filter = {}
+        if (name) {
+            filter.name = name
+        }
+        const users = await User.find(filter);
+        console.log("Users:", users)
+        return users
+    } catch (error) {
+        console.log("Error: getting users", error)
+    }
+}
+
 //TODO: get user by id or ObjectId
+async function getUser(id = "") {
+  try {
+    const user = await User.findById(id)
+      if (!user) {
+        throw new Error("User not found")
+    }
+      console.log("User:", user);
+    return user;
+  } catch (error) {
+    console.log("Error: getting users", error);
+  }
+}
 
 async function run() {
   await mongoose.connect(DATABASE_PATH);
-    console.log("Mongoose has connected to ", DATABASE_PATH);
+  console.log("Mongoose has connected to ", DATABASE_PATH);
     // await insertUser(getRandomUser());
+    // await getUsers("Helen Moore")
+    await getUser("65f43f41103bcbabae849dbe");
   await mongoose.disconnect();
   console.log("Mongoose has disconnected from ", DATABASE_PATH);
 }
